4dc38329f4b0622d4d0644424974953f3cf3afc6
Mick de Graaf <mick@mickdegraaf.nl>
2020-09-05 15:26:56 +0200

    Implemented migration functionality

--- app/src/App.js
+++ app/src/App.js
@@ -1,5 +1,5 @@
 // app/src/App.js
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useAragonApi } from '@aragon/api-react'
 import windows from "./windows.json";
 import {
@@ -11,9 +11,15 @@ import {
   Card,
   TableCell,
   TableHeader,
-  textStyle
+  textStyle,
+  DropDown,
+  TextInput,
+  Field,
+  Button,
 } from '@aragon/ui'
-import { utils, BigNumber } from 'ethers';
+import { Contract, utils, BigNumber } from 'ethers';
+import { providers } from '@aragon/api';
+import { MerkleTree } from './MerkleTree';
 
 // Calcs amount that can still be migrated at this point in time
 function calcAbleToMigrate(vestingStart, vestingEnd, amount, migrated=0) {
@@ -22,7 +28,10 @@ function calcAbleToMigrate(vestingStart, vestingEnd, amount, migrated=0) {
 }
 
 function App() {
-  const {connectedAccount, appState, api} = useAragonApi();
+  const aragonAPI  = useAragonApi();
+  const {connectedAccount, currentApp, appState, api, network} = aragonAPI;
+
+  let contract;
 
   // Calc leafs
   const windowsWithLeafs = windows.map(item => ({
@@ -39,26 +48,98 @@ function App() {
   }));
 
   // Get my windows
-  let myWindows = windowsWithLeafs.filter((value) => value.address == connectedAccount);
+  const myWindows = windowsWithLeafs.filter((value) => value.address == connectedAccount);
+  const selectItems = myWindows.map(item => (`${(new Date(item.timestamp * 1000)).toLocaleString()} - ${(new Date(item.vestedTimestamp * 1000)).toLocaleString()}`));
 
-  // Get amounts already migrated
-  myWindows.map(item => {
-    console.log(api.amountMigratedFromWindow(item.leaf));
-  });
+
+  const [leafs, setLeafs] = useState({});
+  const [selected, setSelected] = useState(0);
+  const [receiver, setReceiver] = useState('');
+  const [receiverValid, setReceiverValid] = useState(true);
+  const [amount, setAmount] = useState(0);
+  const [amountValid, setAmountValid] = useState(true);
+
+
+  // Only execute this when mounting
+  // TODO better handle state
+  useEffect(() => {
+    const fetch = async() => {
+      // Get amounts already migrated
+
+      for (const item of myWindows) {
+        const result = await api.call("amountMigratedFromWindow", item.leaf).toPromise();
+
+        setLeafs({
+          ...leafs,
+          [item.leaf]: result
+        })
+      }
+
+      setReceiver(connectedAccount);
+      setAmount(utils.formatEther(calcAbleToMigrate(myWindows[0].timestamp, myWindows[0].vestedTimestamp, myWindows[0].amount, leafs[myWindows[0].leaf])));
+    }
+
+    fetch();
+  }, [connectedAccount])
+
+  const onReceiverChange = (event) => {
+    setReceiver(event.target.value);
+    setReceiverValid(utils.isAddress(event.target.value));
+  }
+
+  const onAmountChange = (event) => {
+    setAmount(event.target.value);
+    const weiAmount = utils.parseEther(event.target.value);
+    
+    const window = myWindows[selected]
+    const maxAmount = calcAbleToMigrate(window.timestamp, window.vestedTimestamp, window.amount, leafs[window.leaf]);
+
+    setAmountValid(weiAmount.lt(maxAmount));
+  }
+
+  const migrateSubmit = () => {
+    // Construct merkle tree
+    const merkleTree = new MerkleTree(windows.map(item => (
+        utils.solidityKeccak256(
+        ["address", "uint256", "uint256", "uint256"],
+        [
+          item.address,
+          item.amount,
+          item.timestamp,
+          item.vestedTimestamp
+        ]
+      )
+    )));
+    const selectedWindow = myWindows[selected];
+
+    // Create proof
+    const proof = merkleTree.getProof(selectedWindow.leaf);
+
+    console.log(api.migrateVested(receiver, utils.parseEther(amount).toString(), selectedWindow.amount, selectedWindow.timestamp, selectedWindow.vestedTimestamp, proof).toPromise().then(console.log));
+  }
+
+  // If there are no open vesting windows don't show the form
+  if(myWindows.length == 0) {
+    return(
+      <Main>
+        You have no active vesting windows
+      </Main>
+    )
+  }
 
   return (
     <Main>
       <Table
-    header={
-      <TableRow>
-        <TableHeader title="Vesting Start" />
-        <TableHeader title="Vesting End" />
-        <TableHeader title="Total amount" />
-        <TableHeader title="Amount migrated" />
-        <TableHeader title="Able to migrate" />
-      </TableRow>
-    }
-  > 
+        header={
+          <TableRow>
+            <TableHeader title="Vesting Start" />
+            <TableHeader title="Vesting End" />
+            <TableHeader title="Total amount" />
+            <TableHeader title="Amount migrated" />
+            <TableHeader title="Able to migrate" />
+          </TableRow>
+        }
+      >
 
     {
       myWindows.map((item) => (
@@ -73,16 +154,45 @@ function App() {
             <Text>{utils.formatEther(item.amount)}</Text>
           </TableCell>
           <TableCell>
-            <Text>{utils.formatEther(0)}</Text>
+          <Text>{utils.formatEther(leafs[item.leaf] || 0)}</Text>
           </TableCell>
           <TableCell>
-            <Text>{utils.formatEther(calcAbleToMigrate(item.timestamp, item.vestedTimestamp, item.amount, 0))}</Text>
+            <Text>{utils.formatEther(calcAbleToMigrate(item.timestamp, item.vestedTimestamp, item.amount, leafs[item.leaf]))}</Text>
           </TableCell>
         </TableRow>
       ))
     }
   
    </Table>
+
+   <Card width="100%" style={{padding: 20}}>
+    <Field label="Vesting Window" style={{width: "100%"}}>
+      <DropDown
+          items={selectItems}
+          selected={selected}
+          onChange={setSelected}
+          wide
+      />
+    </Field>
+    <Field label="Receiver" style={{width: "100%"}}>
+      <TextInput
+        style={!receiverValid ? {borderColor: "red"} : {}}
+        value={receiver}
+        onChange={onReceiverChange}
+        wide
+      />
+    </Field>
+    <Field label="Amount" style={{width: "100%"}}>
+      <TextInput
+        style={!amountValid ? {borderColor: "red"} : {}}
+        value={amount}
+        onChange={onAmountChange}
+        wide
+      />
+    </Field>
+
+    <Button onClick={migrateSubmit} disabled={!amountValid || !receiverValid} size="large" mode="strong" wide label="Migrate"/>
+   </Card>
 
   </Main>
   )
